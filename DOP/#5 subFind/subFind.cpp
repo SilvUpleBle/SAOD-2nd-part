d4b72092d818a8c293750f2064dbe57911da4708
SilvUpleBle <andreichirckov59@gmail.com>
2023-06-01 16:11:10 +0300

    5ый доп + 10 задание

---
+++ DOP/#5 subFind/subFind.cpp
@@ -0,0 +1,189 @@
+﻿#include <iostream>
+#include <string>
+#include <chrono>
+#include <fstream>
+#include <vector>
+
+size_t naive(const std::string& str, const std::string& sub)
+{
+    size_t n = str.size();
+    size_t m = sub.size();
+    size_t j = 0;
+    for (int i = 0; i < n - m + 1; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            if (str[i + j] != sub[j])
+                break;
+        }
+        if (j == m)
+            return i;
+    }
+    return std::string::npos;
+}
+
+size_t kmp(const std::string& str, const std::string& sub)
+{
+    int m = str.length();
+    int n = sub.length();
+
+    if (n == 0)
+    {
+        return std::string::npos;
+    }
+
+    if (m < n)
+    {
+        return std::string::npos;
+    }
+
+    int* next = new int[n + 1] {0};
+    int j;
+    for (int i = 1; i < n; i++)
+    {
+        j = next[i];
+        while (j > 0 && sub[j] != sub[i]) {
+            j = next[j];
+        }
+
+        if (j > 0 || sub[j] == sub[i]) {
+            next[i + 1] = j + 1;
+        }
+    }
+    
+    j = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (str[i] == sub[j])
+        {
+            if (++j == n) {
+                delete[] next;
+                return i - j + 1;
+            }
+        }
+        else if (j > 0)
+        {
+            j = next[j];
+            i--;    
+        }
+    }
+}
+
+void badCharHeuristic(std::string str, int size, int badchar[256])
+{
+    int i;
+    for (i = 0; i < 256; i++)
+        badchar[i] = -1;
+    for (i = 0; i < size; i++)
+        badchar[(int)str[i]] = i;
+}
+
+size_t search(std::string txt, std::string pat)
+{
+    int m = pat.size();
+    int n = txt.size();
+    int badchar[256];  
+    badCharHeuristic(pat, m, badchar);
+    int s = 0; 
+               
+    while (s <= (n - m))
+    {
+        int j = m - 1;
+        while (j >= 0 && pat[j] == txt[s + j])
+            j--;
+        if (j < 0)
+        {
+            s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
+            return s;
+        }
+        else
+            s += std::max(1, j - badchar[txt[s + j]]);
+    }
+}
+
+
+int main()
+{
+    std::string str, sub = "He won it four times with Montreal";
+    std::ifstream fin("engwiki_ascii.txt", std::ios::binary);
+
+    if (!fin.is_open())
+    {
+        std::cout << "not open!" << std::endl;
+        return 0;
+    }
+    str.append((std::istreambuf_iterator<char>(fin)), std::istreambuf_iterator<char>());
+
+    size_t n = 10;
+    std::vector<size_t> timesVect(n);
+    std::vector<size_t> idx(n);
+
+    std::cout << "kmp" << std::endl;
+    for (size_t i = 0; i < n; i++)
+    {
+        auto firstT = std::chrono::high_resolution_clock::now();
+        auto index = kmp(str, sub);
+        if (index == std::string::npos)
+            std::cout << "not found\n";
+        else
+            idx[i] = index;
+        auto secondT = std::chrono::high_resolution_clock::now();
+        timesVect[i] = std::chrono::duration_cast<std::chrono::milliseconds>(secondT - firstT).count();
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << idx[i] << '\t' << timesVect[i] << std::endl;
+    }
+
+    std::cout << std::endl << "bm" << std::endl;
+    for (size_t i = 0; i < n; i++)
+    {
+        auto firstT = std::chrono::high_resolution_clock::now();
+        auto index = search(str, sub);
+        if (index == std::string::npos)
+            std::cout << "not found\n";
+        else
+            idx[i] = index;
+        auto secondT = std::chrono::high_resolution_clock::now();
+        timesVect[i] = std::chrono::duration_cast<std::chrono::milliseconds>(secondT - firstT).count();
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << idx[i] << '\t' << timesVect[i] << std::endl;
+    }
+
+    std::cout << std::endl << "naive" << std::endl;
+    for (size_t i = 0; i < n; i++)
+    {
+        auto firstT = std::chrono::high_resolution_clock::now();
+        auto index = naive(str, sub);
+        if (index == std::string::npos)
+            std::cout << "not found\n";
+        else
+            idx[i] = index;
+        auto secondT = std::chrono::high_resolution_clock::now();
+        timesVect[i] = std::chrono::duration_cast<std::chrono::milliseconds>(secondT - firstT).count();
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << idx[i] << '\t' << timesVect[i] << std::endl;
+    }
+    
+    
+    std::cout << std::endl << "std::find" << std::endl;
+    for (size_t i = 0; i < n; i++)
+    { 
+        auto firstT = std::chrono::high_resolution_clock::now();
+        auto index = str.find(sub);
+        if (index == std::string::npos)
+            std::cout << "not found\n";
+        else
+            idx[i] = index;
+        auto secondT = std::chrono::high_resolution_clock::now();
+        timesVect[i] = std::chrono::duration_cast<std::chrono::milliseconds>(secondT - firstT).count();
+    }
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << idx[i] << '\t' << timesVect[i] << std::endl;
+    }
+}
---
+++ DZ_10/lzw/lzw.cpp
@@ -0,0 +1,250 @@
+﻿
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <map>
+#include <memory>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+
+using CodeType = std::uint16_t;
+
+namespace globals {
+
+
+    const CodeType dms{ std::numeric_limits<CodeType>::max() };
+
+}
+
+void compress(std::istream& is, std::ostream& os)
+{
+    std::map<std::pair<CodeType, char>, CodeType> dictionary;
+
+
+    const auto reset_dictionary = [&dictionary] {
+        dictionary.clear();
+
+        const long int minc = std::numeric_limits<char>::min();
+        const long int maxc = std::numeric_limits<char>::max();
+
+        for (long int c = minc; c <= maxc; ++c)
+        {
+            const CodeType dictionary_size = dictionary.size();
+
+            dictionary[{globals::dms, static_cast<char> (c)}] = dictionary_size;
+        }
+    };
+
+    reset_dictionary();
+
+    CodeType i{ globals::dms };
+    char c;
+
+    while (is.get(c))
+    {
+
+        if (dictionary.size() == globals::dms)
+            reset_dictionary();
+
+        if (dictionary.count({ i, c }) == 0)
+        {
+
+            const CodeType dictionary_size = dictionary.size();
+
+            dictionary[{i, c}] = dictionary_size;
+            os.write(reinterpret_cast<const char*> (&i), sizeof(CodeType));
+            i = dictionary.at({ globals::dms, c });
+        }
+        else
+            i = dictionary.at({ i, c });
+    }
+
+    if (i != globals::dms)
+        os.write(reinterpret_cast<const char*> (&i), sizeof(CodeType));
+}
+
+void decompress(std::istream& is, std::ostream& os)
+{
+    std::vector<std::pair<CodeType, char>> dictionary;
+
+
+    const auto reset_dictionary = [&dictionary] {
+        dictionary.clear();
+        dictionary.reserve(globals::dms);
+
+        const long int minc = std::numeric_limits<char>::min();
+        const long int maxc = std::numeric_limits<char>::max();
+
+        for (long int c = minc; c <= maxc; ++c)
+            dictionary.push_back({ globals::dms, static_cast<char> (c) });
+    };
+
+    const auto rebuild_string = [&dictionary](CodeType k) -> std::vector<char> {
+        std::vector<char> s;
+
+        while (k != globals::dms)
+        {
+            s.push_back(dictionary.at(k).second);
+            k = dictionary.at(k).first;
+        }
+
+        std::reverse(s.begin(), s.end());
+        return s;
+    };
+
+    reset_dictionary();
+
+    CodeType i{ globals::dms };
+    CodeType k;
+
+    while (is.read(reinterpret_cast<char*> (&k), sizeof(CodeType)))
+    {
+
+        if (dictionary.size() == globals::dms)
+            reset_dictionary();
+
+        if (k > dictionary.size())
+            throw std::runtime_error("invalid compressed code");
+
+        std::vector<char> s;
+
+        if (k == dictionary.size())
+        {
+            dictionary.push_back({ i, rebuild_string(i).front() });
+            s = rebuild_string(k);
+        }
+        else
+        {
+            s = rebuild_string(k);
+
+            if (i != globals::dms)
+                dictionary.push_back({ i, s.front() });
+        }
+
+        os.write(&s.front(), s.size());
+        i = k;
+    }
+
+    if (!is.eof() || is.gcount() != 0)
+        throw std::runtime_error("corrupted compressed file");
+}
+
+
+void print_usage(const std::string& s = "", bool su = true)
+{
+    if (!s.empty())
+        std::cerr << "\nERROR: " << s << '\n';
+
+    if (su)
+    {
+        std::cerr << "\nUsage:\n";
+        std::cerr << "\tprogram -flag input_file output_file\n\n";
+        std::cerr << "Where `flag' is either `c' for compressing, or `d' for decompressing, and\n";
+        std::cerr << "`input_file' and `output_file' are distinct files.\n\n";
+        std::cerr << "Examples:\n";
+        std::cerr << "\tlzw_v3.exe -c license.txt license.lzw\n";
+        std::cerr << "\tlzw_v3.exe -d license.lzw new_license.txt\n";
+    }
+
+    std::cerr << std::endl;
+}
+
+
+int main()
+{
+
+    enum class Mode {
+        Compress,
+        Decompress
+    };
+
+    Mode m;
+
+    std::cout << "compress or decompress? (c/d): ";
+    char mode;
+    std::cin >> mode;
+    std::cout << std::endl;
+
+    if (mode == 'c')
+        m = Mode::Compress;
+    else
+        if (mode == 'd')
+            m = Mode::Decompress;
+        else
+        {
+            print_usage(std::string("flag `") + mode + "' is not recognized.");
+            return EXIT_FAILURE;
+        }
+
+    const std::size_t buffer_size{ 1024 * 1024 };
+
+    const std::unique_ptr<char[]> input_buffer(new char[buffer_size]);
+    const std::unique_ptr<char[]> output_buffer(new char[buffer_size]);
+
+    std::ifstream input_file;
+    std::ofstream output_file;
+
+    std::cout << "input file path: ";
+    std::string path;
+    std::cin >> path;
+    std::cout << std::endl;
+
+    input_file.rdbuf()->pubsetbuf(input_buffer.get(), buffer_size);
+    input_file.open(path, std::ios_base::binary);
+
+    if (!input_file.is_open())
+    {
+        print_usage(std::string("input_file `") + path + "' could not be opened.");
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "input new file path: ";
+    std::string newPath;
+    std::cin >> newPath;
+    std::cout << std::endl;
+
+    output_file.rdbuf()->pubsetbuf(output_buffer.get(), buffer_size);
+    output_file.open(newPath, std::ios_base::binary);
+
+    if (!output_file.is_open())
+    {
+        print_usage(std::string("output_file `") + newPath + "' could not be opened.");
+        return EXIT_FAILURE;
+    }
+
+    try
+    {
+        input_file.exceptions(std::ios_base::badbit);
+        output_file.exceptions(std::ios_base::badbit | std::ios_base::failbit);
+
+        if (m == Mode::Compress)
+            compress(input_file, output_file);
+        else
+            if (m == Mode::Decompress)
+                decompress(input_file, output_file);
+    }
+    catch (const std::ios_base::failure& f)
+    {
+        print_usage(std::string("File input/output failure: ") + f.what() + '.', false);
+        return EXIT_FAILURE;
+    }
+    catch (const std::exception& e)
+    {
+        print_usage(std::string("Caught exception: ") + e.what() + '.', false);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
